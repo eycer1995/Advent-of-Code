51c79088cd4b3239f718a6c4c3407bc350405fd8
Eycer León <eycerleon@fedora.localdomain>
2022-02-17 23:19:55 -0300

    day 3 part 2 works

--- 2015/d3.py
+++ 2015/d3.py
@@ -1,4 +1,4 @@
-elf_directions = open('./2015/d3.txt', 'r').read()
+elf_directions = open('./2015/d3.txt', 'r').read().replace('\n', '')
 # <>^v
 directions = {
     '^': [0, 1],
@@ -11,13 +11,26 @@ houses = set()
 coor_x = coor_y = 0
 houses.add(tuple([coor_x, coor_y]))
 
+santa_x = santa_y = 0
+santa = set()
+santa.add(tuple([coor_x, coor_y]))
+
+robo_x = robo_y = 0
+robo = set()
+robo.add(tuple([coor_x, coor_y]))
+
 for x in range(len(elf_directions)):
-    if elf_directions[x] == '\n':
-        continue
-
+    coor_x += directions[elf_directions[x]][0]
+    coor_y += directions[elf_directions[x]][1]
+    houses.add(tuple([coor_x, coor_y]))
+    if x % 2 == 1:
+        santa_x += directions[elf_directions[x]][0]
+        santa_y += directions[elf_directions[x]][1]
+        santa.add(tuple([santa_x, santa_y]))
     else:
-        coor_x += directions[elf_directions[x]][0]
-        coor_y += directions[elf_directions[x]][1]
-        houses.add(tuple([coor_x, coor_y]))
+        robo_x += directions[elf_directions[x]][0]
+        robo_y += directions[elf_directions[x]][1]
+        robo.add(tuple([robo_x, robo_y]))
 
-print(len(houses))
+print("Day 3 part 1: " + str(len(houses)))
+print("Day 3 part 2: " + str(len(santa.union(robo))))
