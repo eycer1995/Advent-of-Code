fa47114ec5a9e0e0994c019c81123d505ac31f83
Eycer León <eycerleon@fedora.localdomain>
2022-02-27 13:59:17 -0300

    Day 4 complete
    it works but takes too long to find the numbers

--- 2015/d4.py
+++ 2015/d4.py
@@ -1,15 +1,24 @@
 import hashlib
 
-data = "abcdef"
+data = "yzbqklnj"
 # answer is 609043
-find = ""
+find = 0
 leading = False
-i = 0
-# print(hashlib.md5(data.encode('utf-8')).hexdigest())
-
+md5_input = ""
+md5_hash = ""
+five_zeroes = ""
+six_zeroes = ""
 while leading is False:
-    print(i)
-    i += 1
-    if i == 20:
+    md5_input = data + str(find)
+    md5_hash = hashlib.md5(md5_input.encode('utf-8')).hexdigest()
+    print(md5_hash)
+    # print(find)
+    if md5_hash[0] == "0" and md5_hash[1] == "0" and md5_hash[2] == "0" and md5_hash[3] == "0" and md5_hash[4] == "0" and five_zeroes == "":
+        five_zeroes = find
+    if md5_hash[0] == "0" and md5_hash[1] == "0" and md5_hash[2] == "0" and md5_hash[3] == "0" and md5_hash[4] == "0" and md5_hash[5] == "0":
         leading = True
-print("Leading is true")
+        six_zeroes = find
+    find += 1
+print("Day 4 part 1 is: " + str(five_zeroes))
+print("Day 4 part 2 is: " + str(six_zeroes))
+print("Finished")
