3729146d0bd4ceb5c2c81abe2002cdb288f02d8f
Eycer León <eycerleon@fedora.localdomain>
2022-02-27 14:39:50 -0300

    Day 4 time
    This solution takes 43 seconds to solve

--- 2015/d4.py
+++ 2015/d4.py
@@ -1,24 +1,29 @@
 import hashlib
+import time
 
-data = "yzbqklnj"
-# answer is 609043
-find = 0
-leading = False
-md5_input = ""
-md5_hash = ""
-five_zeroes = ""
-six_zeroes = ""
-while leading is False:
-    md5_input = data + str(find)
-    md5_hash = hashlib.md5(md5_input.encode('utf-8')).hexdigest()
-    print(md5_hash)
-    # print(find)
-    if md5_hash[0] == "0" and md5_hash[1] == "0" and md5_hash[2] == "0" and md5_hash[3] == "0" and md5_hash[4] == "0" and five_zeroes == "":
-        five_zeroes = find
-    if md5_hash[0] == "0" and md5_hash[1] == "0" and md5_hash[2] == "0" and md5_hash[3] == "0" and md5_hash[4] == "0" and md5_hash[5] == "0":
-        leading = True
-        six_zeroes = find
-    find += 1
-print("Day 4 part 1 is: " + str(five_zeroes))
-print("Day 4 part 2 is: " + str(six_zeroes))
-print("Finished")
+
+def main():
+    data = "yzbqklnj"
+    find = 0
+    leading = False
+    md5_hash = md5_input = five_zeroes = ""
+    while leading is False:
+        md5_input = data + str(find)
+        md5_hash = hashlib.md5(md5_input.encode('utf-8')).hexdigest()
+        print(md5_hash)
+        # print(find)
+        if md5_hash[0] == "0" and md5_hash[1] == "0" and md5_hash[2] == "0" and md5_hash[3] == "0" and md5_hash[4] == "0" and five_zeroes == "":
+            five_zeroes = find
+            print("Day 4 part 1 is: " + str(five_zeroes))
+        if md5_hash[0] == "0" and md5_hash[1] == "0" and md5_hash[2] == "0" and md5_hash[3] == "0" and md5_hash[4] == "0" and md5_hash[5] == "0":
+            leading = True
+            print("Day 4 part 2 is: " + str(find))
+        find += 1
+
+
+if __name__ == "__main__":
+    t1 = time.perf_counter()
+    main()
+    t2 = time.perf_counter()
+    print("Finished")
+    print(f"Elapsed time: {t2 - t1:0.2f}s")
