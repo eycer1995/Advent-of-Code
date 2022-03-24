2c52caccbd6fd9f292618da55defc4a6f79aadcc
Eycer León <eycerleon@fedora.localdomain>
2022-03-24 16:51:05 -0300

    Better format

--- 2015/d5.py
+++ 2015/d5.py
@@ -4,7 +4,11 @@ data = open('./2015/d5.txt', 'r').read().split('\n')
 def three_vowels(string):
     count = 0
     for letter in string:
-        if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+        if letter == "a" or \
+           letter == "e" or \
+           letter == "i" or \
+           letter == "o" or \
+           letter == "u":
             count += 1
     if count >= 3:
         return True
@@ -24,14 +28,19 @@ def forbidden_pairs(string):
     for letter in range(len(string) - 1):
         pair = string[letter] + string[letter + 1]
         # print(pair)
-        if pair == "ab" or pair == "cd" or pair == "pq" or pair == "xy":
+        if pair == "ab" or \
+           pair == "cd" or \
+           pair == "pq" or \
+           pair == "xy":
             return True
 
 
 nice_counter = 0
 for string in range(len(data)):
     # print(data[string])
-    if appears_twice(data[string]) and three_vowels(data[string]) and not forbidden_pairs(data[string]):
+    if appears_twice(data[string]) and \
+       three_vowels(data[string]) and not \
+       forbidden_pairs(data[string]):
         nice_counter += 1
         print(f"String #{string}: {data[string]} has 3 vowels")
         print("and has a least 1 repeated letter")
