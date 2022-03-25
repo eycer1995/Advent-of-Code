2c9d7becc41752b60791edc97ffd277f2094a88b
Eycer León <eycerleon@fedora.localdomain>
2022-03-25 17:26:09 -0300

    Finished 2015 Day 5 part 2

--- 2015/d5.py
+++ 2015/d5.py
@@ -1,3 +1,4 @@
+import time
 data = open('./2015/d5.txt', 'r').read().split('\n')
 
 
@@ -15,12 +16,9 @@ def three_vowels(string):
 
 
 def appears_twice(string):
-    count = 0
     for letter in range(len(string)):
         if letter >= 1 and string[letter - 1] == string[letter]:
-            count += 1
-    if count >= 1:
-        return True
+            return True
 
 
 def forbidden_pairs(string):
@@ -35,16 +33,49 @@ def forbidden_pairs(string):
             return True
 
 
-nice_counter = 0
-for string in range(len(data)):
-    # print(data[string])
-    if appears_twice(data[string]) and \
-       three_vowels(data[string]) and not \
-       forbidden_pairs(data[string]):
-        nice_counter += 1
-        print(f"String #{string}: {data[string]} has 3 vowels")
-        print("and has a least 1 repeated letter")
-        print(f"String #{string}:{data[string]} does not a permitted pair")
-        print("--------------------------")
+def main1():
+    nice_counter = 0
+    for string in range(len(data)):
+        # print(data[string])
+        if appears_twice(data[string]) and \
+            three_vowels(data[string]) and not \
+            forbidden_pairs(data[string]):
+            nice_counter += 1
+            # print(f"String #{string}: {data[string]} has 3 vowels")
+            # print("has a least 1 repeated letter")
+            # print("and does not have a forbidden pair")
+            # print("--------------------------")
+    print(f"There are {nice_counter} nice words part 1.")
 
-print(f"There are {nice_counter} nice words.")
+
+# Day 5 part 2 ------------------------------
+def double_pair(string):
+    for letter in range(len(string)):
+        if string[letter:letter+2] in string[letter+2:]:
+            return True
+    return False
+
+
+def middle_letter(string):
+    for letter in range(len(string) - 2):
+        if string[letter] == string[letter + 2]:
+            return True
+
+
+def main2():
+    nicest_counter = 0
+    for string in range(len(data)):
+        if double_pair(data[string]) and middle_letter(data[string]):
+            nicest_counter += 1
+            # print(f"String #{string}: {data[string]} has a double pair")
+            # print("------------------------")
+    print(f"There are {nicest_counter} words in part 2.")
+
+
+if __name__ == "__main__":
+    t1 = time.perf_counter()
+    main1()
+    main2()
+    t2 = time.perf_counter()
+    print("Finished")
+    print(f"Elapsed time: {t2 - t1:0.2f}s")
