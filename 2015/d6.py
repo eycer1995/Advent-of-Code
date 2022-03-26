139d9c63db721d0220ff471678b82a4972829297
Eycer León <eycerleon@fedora.localdomain>
2022-03-26 20:40:41 -0300

    Day 6 part 2 complete

--- 2015/d6.py
+++ 2015/d6.py
@@ -4,36 +4,47 @@ data = open("./2015/d6.txt", "r").read().split("\n")
 
 def arrange_light():
     global lights
+    global bright_lights
     lights = []
+    bright_lights = []
     for x in range(1000):
         lights.append([])
+        bright_lights.append([])
         for y in range(1000):
-            lights[x].append("off")
+            lights[x].append(False)
+            bright_lights[x].append(0)
 
 
 def count_on():
     count = 0
     for x in range(1000):
         for y in range(1000):
-            if lights[x][y] == "on":
+            if lights[x][y]:
                 count += 1
     return count
 
 
+def count_brightness():
+    count = 0
+    for x in range(1000):
+        for y in range(1000):
+            count = count + bright_lights[x][y]
+    return count
+
+
 def change_lights(nstate, coor1, coor2):
-    for x in range(coor1[0], coor2[0]):
-        for y in range(coor1[1], coor2[1]):
+    for x in range(coor1[0], coor2[0] + 1):
+        for y in range(coor1[1], coor2[1] + 1):
             if nstate == "toggle":
-                if lights[x][y] == "off":
-                    lights[x][y] = "on"
-                else:
-                    lights[x][y] = "off"
+                bright_lights[x][y] += 2
+                lights[x][y] = not lights[x][y]
             elif nstate == "off":
-                if lights[x][y] == "on":
-                    lights[x][y] = "off"
+                if bright_lights[x][y] > 0:
+                    bright_lights[x][y] -= 1
+                lights[x][y] = False
             elif nstate == "on":
-                if lights[x][y] == "off":
-                    lights[x][y] = "on"
+                bright_lights[x][y] += 1
+                lights[x][y] = True
 
 
 def get_instruction(instruction):
@@ -65,7 +76,8 @@ def main():
     arrange_light()
     for instruction in range(len(data) - 1):
         get_instruction(data[instruction])
-        # print(f"There are {count_on()} lights on.")
+    print(f"There are {count_on()} lights on.")
+    print(f"There is {count_brightness()} total brightness.")
 
 
 if __name__ == "__main__":
