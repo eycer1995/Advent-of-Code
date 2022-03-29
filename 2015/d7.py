6289dd44e583a607cf83fd1f9c79b5fda9756c70
Eycer León <eycerleon@fedora.localdomain>
2022-03-29 20:06:03 -0300

    Day 7 start
    using objects

---
+++ 2015/d7.py
@@ -0,0 +1,45 @@
+import time
+
+
+class Wire():
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+
+def get_data():
+    lines = []
+    with open("./2015/d7.txt", "r") as f:
+        x = f.readline()
+        while(x):
+            lines.append(x)
+            x = f.readline()
+        return [(['',''] + l.replace('\n','').split(' '))[-5:] for l in lines]
+
+
+def build(wire_a, wire_b):
+    wires = []
+    if 
+
+
+
+def proccess_data(operation):
+    if "AND" in operation:
+        
+
+
+def main():
+    data = get_data()
+    for i in range(len(data)):
+        print(data[i])
+
+
+if __name__ == "__main__":
+    t1 = time.perf_counter()
+    main()
+    t2 = time.perf_counter()
+    print("Finished")
+    print(f"Elapsed time: {t2 - t1:0.2f}s")
+
+
+
