c64c817ce18b1407f9f29f99f31a6ae53a550ab2
Eycer <eycerleon@gmail.com>
2022-12-15 22:01:09 -0300

    2022 d2 part 2

--- 2022/python/d2.py
+++ 2022/python/d2.py
@@ -13,6 +13,11 @@
 # outcome of the round 
 # (0 if you lost, 3 if the round was a draw, and 6 if you won)
 
+# Part 2
+# X meas to lose
+# Y measn to draw
+# Z means to win
+
 data = open('./inputs/d2.txt', 'r').read().split("\n")
 
 dic = {
@@ -24,12 +29,26 @@ dic = {
     "Z": "Scissors"
 }
 
+dic_2 = {
+    "X": "lose",
+    "Y": "draw",
+    "Z": "win",
+}
+
+
 score = 0
+score_2 = 0
 
 for i in range(len(data) - 1):
-    #print(dic[data[i][0]])
+    # My play following the strategy 
+
     elf_play = dic[data[i][0]]
     my_play = dic[data[i][2]]
+
+    # My play if not following the strategy 
+    my_play_2 = dic_2[data[i][2]]
+
+    
     if (elf_play == "Rock") and (my_play == "Paper"):
         score = score + 2 + 6
     elif (elf_play == "Scissors") and (my_play == "Paper"):
@@ -51,5 +70,26 @@ for i in range(len(data) - 1):
     else:
         print("condition error")
 
-print("Following the strategy guide: " + str(score))
+    if (elf_play == "Rock") and (my_play_2 == "win"):
+        score_2 = score_2 + 2 + 6
+    elif (elf_play == "Rock") and (my_play_2 == "lose"):
+        score_2 = score_2 + 3 + 0 
+    elif (elf_play == "Rock") and (my_play_2 == "draw"):
+        score_2 = score_2 + 1 + 3
+    elif (elf_play == "Scissors") and (my_play_2 == "win"):
+        score_2 = score_2 + 1 + 6
+    elif (elf_play == "Scissors") and (my_play_2 == "lose"):
+        score_2 = score_2 + 2 + 0
+    elif (elf_play == "Scissors") and (my_play_2 == "draw"):
+        score_2 = score_2 + 3 + 3
+    elif (elf_play == "Paper") and (my_play_2 == "win"):
+        score_2 = score_2 + 3 + 6
+    elif (elf_play == "Paper") and (my_play_2 == "lose"):
+        score_2 = score_2 + 1 + 0
+    elif (elf_play == "Paper") and (my_play_2 == "draw"):
+        score_2 = score_2 + 2 + 3
+    else:
+        print("cond 2 error")
 
+print("Following the strategy guide: " + str(score))
+print("If not following the strategy: " + str(score_2))
